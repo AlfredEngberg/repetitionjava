0cbd2d8f8a2727d1b0b8cfd194b87f0c0657000b
AlfredEngberg <alfred.engberg@elev.ga.ntig.se>
2023-04-18 15:07:09 +0200

    fixade så att den laddar in filen in i en arraylsit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
     }
-}
+
+    public static ArrayList<String> filereader() {
+        ArrayList<String> logfil = new ArrayList<>();
+
+         Scanner fil;
+
+            try {
+                fil = new Scanner(new FileReader("ideaold.log"));
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            while (fil.hasNext()) {
+                logfil.add(fil.nextLine());
+
+            }
+            return logfil;
+        }
+    }
+
